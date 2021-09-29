539c243f5e95b6c3ab8d6b8999233b187d3ab019
mateusmoreira32 <74328621+mateusmoreira32@users.noreply.github.com>
2021-09-29 18:52:46 -0300

    Create automato.py

---
+++ automato.py
@@ -0,0 +1,40 @@
+import ply.lex as lex
+
+tokens = (
+    'NUMBER',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'LPAREN',
+    'RPAREN',
+    'NOME',
+    'IGUAL',
+    'FINALLINHA',
+)
+
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_NOME = r'\w+'
+t_IGUAL = r'='
+t_FINALLINHA = r';'
+
+
+def t_NUMBER(t):
+    r'(\d*\.\d+)|(\d+\.\d*)'
+    t.value =float(t.value)
+    return t
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+t_ignore = ' \t'
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+lexer = lex.lex()
